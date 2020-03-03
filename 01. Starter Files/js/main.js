0d801383006eef6a8b357125d9abffa7e699b27e
vianney <60422994+SlothVia@users.noreply.github.com>
2020-03-03 15:34:00 +0100

    first commit

---
+++ 01. Starter Files/js/main.js
@@ -0,0 +1,9 @@
+var noun = prompt ("Enter your favorite outdoor sport: ");
+
+var verb = prompt ("Enter your favorite indoor sport: ");
+
+var adjective = prompt ("Enter your favorite athlete: ");
+
+var adverb = prompt ("Enter your favorite coach: ");
+
+document.write("Do you like playing "+verb+" with " +adjective+ " and playing " +noun+ " with " +adverb+ " ? That\'s hilarious");
